c78ad32db505f86e099639343a3812eefdb0f492
Zia <ziamohseni2013@gmail.com>
2024-04-16 11:05:11 +0200

    Mobile app init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ii1302-Project
+
+In this repository we will have both the mobile application and the Raspberry Pi code under respective folder.
+
+### FSS-APP
+
+This folder is the mobile app. Do not edit unless you work with mobile app.
---
+++ fss-app/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>5Sense Security.</Text>
+      <Text>home security for every home!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
