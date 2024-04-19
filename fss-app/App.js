7f2d9021b8b656a7b77b7135d1674d165c27e4aa
Zia <ziamohseni2013@gmail.com>
2024-04-19 16:26:10 +0200

    Added icons to tab navigation and styled it

--- fss-app/App.js
+++ fss-app/App.js
@@ -1,21 +1,74 @@
 import { StatusBar } from "expo-status-bar";
+import { Platform } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+// Styles
+import globalStyles from "./src/styles/globalStyles";
 
 // Screens
-import HomeScreen from "./screens/HomeScreen";
-import DevicesScreen from "./screens/DevicesScreen";
-import ProfileScreen from "./screens/ProfileScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import DevicesScreen from "./src/screens/DevicesScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Devices" component={DevicesScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          tabBarActiveTintColor: globalStyles.primaryColor.color,
+          tabBarInactiveTintColor: globalStyles.darkColor.color,
+          tabBarStyle: {
+            backgroundColor: globalStyles.lightColor.color,
+            borderTopColor: globalStyles.borderColor.borderColor,
+            height: Platform.OS === "ios" ? 84 : 54,
+            paddingTop: 5,
+          },
+          tabBarLabelStyle: {
+            paddingBottom: 5,
+          },
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ focused, color }) =>
+              focused ? (
+                <Ionicons name="home" size={26} color={color} />
+              ) : (
+                <Ionicons name="home-outline" size={26} color={color} />
+              ),
+          }}
+        />
+        <Tab.Screen
+          name="Devices"
+          component={DevicesScreen}
+          options={{
+            tabBarIcon: ({ focused, color }) =>
+              focused ? (
+                <Ionicons name="cog" size={26} color={color} />
+              ) : (
+                <Ionicons name="cog-outline" size={26} color={color} />
+              ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({ focused, color }) =>
+              focused ? (
+                <Ionicons name="person" size={26} color={color} />
+              ) : (
+                <Ionicons name="person-outline" size={26} color={color} />
+              ),
+          }}
+        />
       </Tab.Navigator>
       <StatusBar />
     </NavigationContainer>
---
+++ fss-app/src/styles/globalStyles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const globalStyles = StyleSheet.create({
+  lightColor: {
+    color: "#ededed",
+  },
+  primaryColor: {
+    color: "#1E90FF",
+  },
+  container: {
+    flex: 1,
+  },
+  darkColor: {
+    color: "#111111",
+  },
+  borderColor: {
+    borderColor: "#b5b5b5",
+  },
+});
+
+export default globalStyles;
