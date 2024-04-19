ba7839f41786416e143249210cbf5e170e65867b
Zia <ziamohseni2013@gmail.com>
2024-04-19 22:10:11 +0200

    Added a simple signout button in profile screen

--- fss-app/src/screens/ProfileScreen.js
+++ fss-app/src/screens/ProfileScreen.js
@@ -1,9 +1,23 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
+import { signOut } from "firebase/auth";
+import { auth } from "../services/firebaseConfig";
 
 function ProfileScreen() {
+  // Function to handle sign out
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("Sign out successful");
+      })
+      .catch((error) => {
+        console.log("Sign out failed");
+      });
+  };
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Profile!</Text>
+      <Text>Profile screen!</Text>
+      <Button title="Sign Out" onPress={handleSignOut} />
     </View>
   );
 }
