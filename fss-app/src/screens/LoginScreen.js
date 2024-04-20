b05a6fa7395a750fa92cdb3b759f750579e3acc2
Zia <ziamohseni2013@gmail.com>
2024-04-20 09:35:53 +0200

    Seperated login screen styles

--- fss-app/src/screens/LoginScreen.js
+++ fss-app/src/screens/LoginScreen.js
@@ -15,23 +15,13 @@ import logo from "../../assets/fss-logo.png";
 
 // Styles
 import globalStyles from "../styles/globalStyles";
+import styles from "../styles/loginScreenStyles";
 
 function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        console.log("User is signed in!");
-      } else {
-        console.log("User is signed out!");
-      }
-    });
-
-    return unsubscribe;
-  }, []);
-
+  // Login function
   const handleLogin = async () => {
     try {
       const userCredential = await signInWithEmailAndPassword(
@@ -82,60 +72,3 @@ function LoginScreen() {
 }
 
 export default LoginScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    paddingHorizontal: 20,
-    backgroundColor: globalStyles.lightColor.color,
-  },
-  logoContainer: {
-    alignItems: "center",
-    marginBottom: 30,
-  },
-  input: {
-    fontWeight: "bold",
-    backgroundColor: "#eeeeee",
-    color: globalStyles.darkColor.color,
-    height: 65,
-    borderColor: globalStyles.secondLightColor.color,
-    borderWidth: 1,
-    borderRadius: 15,
-    marginVertical: 10,
-    padding: 10,
-    shadowColor: globalStyles.darkColor.color,
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.15,
-    shadowRadius: 3.84,
-    elevation: 25,
-  },
-  buttonActive: {
-    alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: 20,
-    paddingHorizontal: 10,
-    marginTop: 30,
-    marginBottom: 15,
-    borderRadius: 10,
-    elevation: 3,
-    backgroundColor: globalStyles.primaryColor.color,
-  },
-  buttonDisabled: {
-    alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: 20,
-    paddingHorizontal: 10,
-    marginTop: 30,
-    borderRadius: 10,
-    elevation: 3,
-    backgroundColor: globalStyles.secondLightColor.color,
-  },
-  text: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: "bold",
-    letterSpacing: 0.25,
-    color: "white",
-  },
-});
---
+++ fss-app/src/styles/loginScreenStyles.js
@@ -0,0 +1,61 @@
+import { StyleSheet } from "react-native";
+import globalStyles from "./globalStyles";
+
+const loginScreenStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    backgroundColor: globalStyles.lightColor.color,
+  },
+  logoContainer: {
+    alignItems: "center",
+    marginBottom: 30,
+  },
+  input: {
+    fontWeight: "bold",
+    backgroundColor: "#eeeeee",
+    color: globalStyles.darkColor.color,
+    height: 65,
+    borderColor: globalStyles.secondLightColor.color,
+    borderWidth: 1,
+    borderRadius: 15,
+    marginVertical: 10,
+    padding: 10,
+    shadowColor: globalStyles.darkColor.color,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.15,
+    shadowRadius: 3.84,
+    elevation: 25,
+  },
+  buttonActive: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    marginTop: 30,
+    marginBottom: 15,
+    borderRadius: 10,
+    elevation: 3,
+    backgroundColor: globalStyles.primaryColor.color,
+  },
+  buttonDisabled: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    marginTop: 30,
+    borderRadius: 10,
+    elevation: 3,
+    backgroundColor: globalStyles.secondLightColor.color,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+});
+
+export default loginScreenStyles;
