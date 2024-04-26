c3f3c3a6b1c8b51d0082cd65c3aacff2c86ede19
AntonLittholt <132568692+AntonLittholt@users.noreply.github.com>
2024-04-26 13:00:38 +0200

    Forgot password component
    Now forgot password component looks good and is consistent with the login screen.

--- fss-app/src/components/loginscreen/ResetPassword.js
+++ fss-app/src/components/loginscreen/ResetPassword.js
@@ -1,21 +1,38 @@
 import {Text, View, TextInput, TouchableOpacity, Alert} from "react-native"
 import { sendPasswordResetEmail } from "firebase/auth";
 import { auth } from "../../services/firebaseConfig";
-import {useState} from "react"
+import {useState, useEffect} from "react"
 
 //styles
 import styles from "../../styles/resetPasswordStyles";
+import globalStyles from "../../styles/globalStyles";
 
 function ResetPassword(props) {
 
   const [email, setEmail] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(true);
+  const [isButtonActive, setIsButtonActive] = useState(false);
+
+  useEffect(() => {
+    const emailRegex = /\S+@\S+\.\S+/;
+    setIsValidEmail(emailRegex.test(email));
+  }, [email]);
+
+  useEffect(() => {
+    if (email.length > 0 && isValidEmail) {
+      setIsButtonActive(true);
+    } else {
+      setIsButtonActive(false);
+    }
+  }, [email]);
 
   function resetPassword(){
 
     sendPasswordResetEmail(auth, email)
       .then(() => {
         // Password reset email sent!
-        Alert.alert("Email sent!", "Look in your email inbox");
+        Alert.alert("Email sent!", "Check your mail inbox! You will see mail from 5Sense Security that conatins a link which enables you to reset the password for your account. Check spam folder if you cant find the mail");
+
         backToLogin();
       })
       .catch((error) => {
@@ -44,7 +61,7 @@ function ResetPassword(props) {
       </View>
 
       <TextInput 
-          style = {styles.input}
+          style = {globalStyles.inputStyle}
           autoCapitalize="none"
           value={email}
           inputMode="email"
@@ -52,20 +69,28 @@ function ResetPassword(props) {
           placeholder="Email address"
           onChangeText={(text) => setEmail(text)}
       />
+      {email.length > 0 && !isValidEmail && (
+        <Text style={globalStyles.errorText}>Email is not valid.</Text>
+      )}
 
       <TouchableOpacity 
-        style = {styles.button} 
+        style={
+          isButtonActive
+            ? globalStyles.buttonActive
+            : globalStyles.buttonDisabled
+        }
+        disabled={!isButtonActive}
         onPress={() => {
           resetPassword();
           clearInput();
         }}
       >
-        <Text style = {styles.text} > Send email </Text>
+        <Text style = {isButtonActive ? styles.text : styles.textInactive} > Send email </Text>
       </TouchableOpacity>
 
       <View style = {styles.backButtonContainer}>
         <TouchableOpacity onPress = {backToLogin} style = {styles.backButton}>
-          <Text style = {styles.backButtonIcon}>Back to login </Text>
+          <Text style = {styles.backButtonIcon}>BACK TO LOGIN</Text>
         </TouchableOpacity>
       </View>
 
