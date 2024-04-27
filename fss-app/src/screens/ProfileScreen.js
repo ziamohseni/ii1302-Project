2c426f1f15d5413a61595de2d4e27a4319eabfa0
Zia <ziamohseni2013@gmail.com>
2024-04-27 11:39:48 +0200

    Added an example usage of useRaspberryHubs context

--- fss-app/src/screens/ProfileScreen.js
+++ fss-app/src/screens/ProfileScreen.js
@@ -3,10 +3,14 @@ import { signOut } from "firebase/auth";
 import { auth } from "../services/firebaseConfig";
 // Context
 import { useUser } from "../contexts/UserContext";
+import { useRaspberryHubs } from "../contexts/RaspberryHubsContext";
 
 function ProfileScreen() {
   // Get user data from context
   const { user, profile } = useUser();
+  // Get Raspberry Hubs data from context
+  const { hubs, selectHub } = useRaspberryHubs();
+
   // Function to handle sign out
   const handleSignOut = () => {
     signOut(auth)
@@ -25,6 +29,15 @@ function ProfileScreen() {
       <Text>First Name: {profile?.first_name}</Text>
       <Text>Last Name: {profile?.last_name}</Text>
       <Button title="Sign Out" onPress={handleSignOut} />
+      <View>
+        <Text>Raspberry Hubs:</Text>
+        {hubs.map((hub) => (
+          <View key={hub.id}>
+            <Text>Hub ID: {hub.id}</Text>
+            <Button title="Select Hub" onPress={() => selectHub(hub.id)} />
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
