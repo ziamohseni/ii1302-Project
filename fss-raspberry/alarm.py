1f5eeb140fddb6c1189b89a8a8aab7bffd2070af
Adalet Adiljan <130295885+Adaleet@users.noreply.github.com>
2024-05-03 19:59:38 +0200

    Update alarm.py

--- fss-raspberry/alarm.py
+++ fss-raspberry/alarm.py
@@ -1,22 +1,28 @@
 import os # Since there's no inbuilt way to stop a sound played using 'playsound' library, we can use 'os' module to stop the sound
           # The 'os' module can be used to stop the sound, by terminating the process responsible for playing it.
 
+import multiprocessing
 from playsound import playsound
+
 # Make sure to install the playsound library by running ' pip install playsound ' . 
 class Alarm:
     def __init__(self, sound_file):
         self.sound_file = sound_file
+        self.process = None
 
     def start(self):
-        playsound(self.sound_file)
+        # Start the alarm sound in a separate process
+        self.process = multiprocessing.Process(target=self._play_sound)
+        self.process.start()
 
     def stop(self):
-        # There's no built-in way to stop a sound played using playsound
-        # You can implement your own logic to handle stopping the sound if needed
-        if self.process_id:
-        # Terminate the process responsible for playing the sound
-        os.system(f"kill {self.process_id}")
+        if self.process:
+            # Terminate the process responsible for playing the sound
+            self.process.terminate()
 
+    def _play_sound(self):
+        playsound(self.sound_file)
+'''
 def main(): # Example of how we can use the alarm sound in the main function
 
     alarm_sound_file = "alarm.mp3" # Sound file can be found in the fss-raspberry map. This is the path to the alarm sound file
@@ -33,4 +39,4 @@ def main(): # Example of how we can use the alarm sound in the main function
 
 if __name__ == "__main__":
     main()
-
+'''
