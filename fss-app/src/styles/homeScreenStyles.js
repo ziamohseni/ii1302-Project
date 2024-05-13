e8d03e846bb522421b5d811ef372061acc786e77
AntonLittholt <132568692+AntonLittholt@users.noreply.github.com>
2024-05-13 21:15:03 +0200

    Added last_armed info to homeScreen

--- fss-app/src/styles/homeScreenStyles.js
+++ fss-app/src/styles/homeScreenStyles.js
@@ -11,6 +11,27 @@ const homeScreenStyles = StyleSheet.create({
     padding: 20,
     paddingBottom: Platform.OS === "ios" ? 30 : 50,
   },
+  imageContainer:{
+    paddingRight: 20,
+  },
+  lastArmedContainer:{
+    justifyContent: "center",
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: globalStyles.secondLightColor.color,
+    paddingHorizontal: 10,
+    borderRadius: 15,
+    alignItems: "center",
+    paddingRight: 20,
+  },
+  lastArmedText:{
+    textAlign: "left",
+    fontSize: 16,
+  },
+  timeText:{
+    fontSize: 16,
+    fontWeight: "bold"
+  }
 });
 
 export default homeScreenStyles;
--- fss-app/src/styles/profileInfoStyles.js
+++ fss-app/src/styles/profileInfoStyles.js
@@ -4,10 +4,8 @@ import globalStyles from "./globalStyles";
 const profileInfoStyles = StyleSheet.create({
   container: {
     backgroundColor: globalStyles.lightColor.color,
-    //padding: 15,
-    paddingTop: 15,
+    paddingTop: 15, 
     borderRadius: 15,
-    marginTop: 20,
     borderColor: globalStyles.secondLightColor.color,
     borderWidth: 1,
   },
