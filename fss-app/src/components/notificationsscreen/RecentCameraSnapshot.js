ac204b8ff3396d056d1e177cd2c89775ce0f3b91
Zia <ziamohseni2013@gmail.com>
2024-05-14 11:41:32 +0200

    Added activity indicator to recent camera snapshot component

--- fss-app/src/components/notificationsscreen/RecentCameraSnapshot.js
+++ fss-app/src/components/notificationsscreen/RecentCameraSnapshot.js
@@ -1,7 +1,13 @@
 import { useEffect, useState } from "react";
 import { ref, getDownloadURL } from "firebase/storage";
 import { storage } from "../../services/firebaseConfig";
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  ActivityIndicator,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useRaspberryHubs } from "../../contexts/RaspberryHubsContext";
 import convertUnixTimestampToDate from "../../utils/convertUnixTimestampToDate";
@@ -12,6 +18,7 @@ import styles from "../../styles/notificationsHistoryScreenStyles";
 const RecentCameraSnapshot = () => {
   const navigation = useNavigation();
   const { selectedHub, noHubsFound } = useRaspberryHubs();
+  const [loading, setLoading] = useState(true);
   const [recentCameraSnapshotUrl, setRecentCameraSnapshotUrl] = useState(null);
   const [recentCameraSnapshotInfo, setRecentCameraSnapshotInfo] =
     useState(null);
@@ -20,12 +27,14 @@ const RecentCameraSnapshot = () => {
   useEffect(() => {
     // Function Get recent camera snapshot
     const getRecentCameraSnapshot = async () => {
+      setLoading(true);
       if (selectedHub) {
         const recentSnapshot = selectedHub.sensors.camera.recent_snapshot;
         const storageRef = ref(storage, recentSnapshot.path);
         const url = await getDownloadURL(storageRef);
         setRecentCameraSnapshotUrl(url);
         setRecentCameraSnapshotInfo(recentSnapshot);
+        setLoading(false);
       }
     };
     getRecentCameraSnapshot();
@@ -41,7 +50,8 @@ const RecentCameraSnapshot = () => {
       onPress={() => navigation.navigate("CameraHistoryModal")}
       style={styles.recentCameraSnapshotContainer}
     >
-      {recentCameraSnapshotUrl ? (
+      {/* Recent snapshot */}
+      {recentCameraSnapshotUrl && !loading ? (
         <View style={styles.recentCameraSnapshot}>
           <Image
             source={{ uri: recentCameraSnapshotUrl }}
@@ -62,7 +72,15 @@ const RecentCameraSnapshot = () => {
         </View>
       ) : (
         <View style={styles.noRecentCameraSnapshot}>
-          <Text>No recent camera snapshot found</Text>
+          {loading ? (
+            <ActivityIndicator
+              size="large"
+              color={globalStyles.darkColor.color}
+              style={{ marginTop: 50, alignSelf: "center" }}
+            />
+          ) : (
+            <Text>No recent camera snapshot found</Text>
+          )}
         </View>
       )}
     </TouchableOpacity>
