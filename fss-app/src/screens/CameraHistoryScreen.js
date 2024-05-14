88ce23ba6ceb78de7b9a488178e88f6d3277f195
Zia <ziamohseni2013@gmail.com>
2024-05-14 10:43:35 +0200

    Added style to camera history screen and also added recent camera snapshot component to it

--- fss-app/src/screens/CameraHistoryScreen.js
+++ fss-app/src/screens/CameraHistoryScreen.js
@@ -1,26 +1,42 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import RecentCameraSnapshot from "../components/notificationsscreen/RecentCameraSnapshot";
 // Styles
 import globalStyles from "../styles/globalStyles";
+import styles from "../styles/cameraHistoryScreenStyles";
 
 const CameraHistoryScreen = () => {
   const navigation = useNavigation();
 
   return (
-    <View>
-      <Text>CameraHistoryScreen</Text>
-      {/* Close button */}
-      <View>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Ionicons
-            name="close-circle-outline"
-            size={60}
-            color={globalStyles.lightColor.color}
-          />
-        </TouchableOpacity>
+    <SafeAreaView style={styles.outerContainer}>
+      <View style={styles.innerContainer}>
+        <ScrollView contentContainerStyle={styles.content}>
+          {/* Recent Snapshot */}
+          <Text style={styles.title}>Recent Camera Snapshot</Text>
+          <RecentCameraSnapshot />
+          {/* Camera History */}
+          <Text style={styles.title}>Camera History</Text>
+        </ScrollView>
+        {/* Close button */}
+        <View style={styles.closeButton}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Ionicons
+              name="close-circle-outline"
+              size={60}
+              color={globalStyles.darkColor.color}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
---
+++ fss-app/src/styles/cameraHistoryScreenStyles.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+import globalStyles from "./globalStyles";
+
+//platform
+import { Platform } from "react-native";
+
+const devicesScreenStyles = StyleSheet.create({
+  outerContainer: {
+    flex: 1,
+    position: "relative",
+    backgroundColor: globalStyles.lightColor.color,
+  },
+  innerContainer: {
+    flex: 1,
+    flexDirection: "column",
+    gap: 20,
+    marginTop: Platform.OS === "ios" ? 10 : 40,
+    paddingHorizontal: 20,
+  },
+  content: {
+    flex: 1,
+    flexDirection: "column",
+    gap: 20,
+    flexGrow: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: globalStyles.darkColor.color,
+  },
+  closeButton: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default devicesScreenStyles;
